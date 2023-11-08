0222bee603377284b723ad55a0c052091668f940
marquis brunbdage <marquisbrunbdage@marquiss-MacBook-Pro.local>
2023-11-07 21:21:38 -0500

    set up code

---
+++ index.js
@@ -0,0 +1,6 @@
+const LISTING_CONTENT = document.getElementsByClassName("listingcontainer");
+const USER_LISTINGS = document.getElementById("listings");
+const  LIST_OF_USERS = document.getElementsByClassName('userlist')
+
+
+
