f53e5dbd3d236a5e9f3c708086d8660024d97b44
marquis brunbdage <marquisbrunbdage@marquiss-MacBook-Pro.local>
2023-11-09 19:48:03 -0500

    addednew fetchEvents

--- index.js
+++ index.js
@@ -34,10 +34,59 @@
 //     loaction: "670 Maysberry Alv",
 //   },
 
+{
+  /* <h1>Party List</h1>
+
+<!-- List of parties -->
+<ul id="partyList"></ul>
+
+<!-- Form to add a new party -->
+<form id="partyForm" method="POST">
+  <input
+    type="text"
+    name="partyName"
+    id="partyName"
+    placeholder="Party Name"
+    required
+  />
+  <input type="date" id="partyDate" required />
+  <input type="time" id="partyTime" required />
+  <input type="text" id="partyLocation" placeholder="Location required" />
+  <textarea
+    name=""
+    id="partyDescription"
+    placeholder="Party Description"
+    cols="5"
+    rows="5"
+    required
+  ></textarea>
+  <button type="submit">Schedule Party</button>
+</form> */
+}
 
 const BASE_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-et-web-pt-b`;
-const EVENTS  = `${BASE_URL}/events`;
+const EVENTS = `${BASE_URL}/events`;
 
+const FORM = document.querySelector("form");
+FORM.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const elements = FORM.elements;
+  const partyName = elements["partyName"].value;
+  const partyDate = elements["partyDate"].value;
+  const partyTime = elements["partyTime"].value;
+  const partyLocation = elements["partyLocation"].value;
+  const partyDescription = elements["partyDescription"].value;
+
+  const newPartyData = {
+    name: partyName,
+    date: `${partyDate}T${partyTime}:00Z`,
+    location: partyLocation,
+    description: partyDescription,
+  };
+  console.log(newPartyData);
+  await createEvent(newPartyData);
+  fetchEvents();
+});
 
 function createPartyCard(tittle, fullName, date, address, description) {
   const PARTY_CARDS = document.querySelector("#cards");
@@ -46,7 +95,7 @@ function createPartyCard(tittle, fullName, date, address, description) {
   const PARTY_CARD_TITTLE = document.createElement("div");
   PARTY_CARD_TITTLE.textContent = tittle;
   PARTY_CARD_TITTLE.classList.add("tittle");
-  const PARTY_CARD_NAME = createElement("p");
+  const PARTY_CARD_NAME = document.createElement("p");
   PARTY_CARD_NAME.textContent = fullName;
   PARTY_CARD_NAME.classList.add("fullName");
   const PARTY_CARD_DATE = document.createElement("p");
@@ -68,36 +117,52 @@ function createPartyCard(tittle, fullName, date, address, description) {
   );
 
   PARTY_CARDS.append(PARTY_CARD);
-  };
+}
 
-  function renderPartyCards(parties){
-    for (const party of parties) {
-        createPartyCard(party.name, party.date, party.address, party.description);
-      }
-    
+function renderPartyCards(parties) {
+  for (const party of parties) {
+    createPartyCard(party.name, party.date, party.address, party.description);
   }
+}
 
-
-async function fetchEvents(){
-try {
+async function fetchEvents() {
+  try {
     //pausing to get a response
     const response = await fetch(EVENTS);
-    if(!response.ok){
-     console.log(`API error`, response.status)
-      return
+    if (!response.ok) {
+      console.log(`API error`, response.status);
+      return;
     }
     const jsonResponse = await response.json();
     const parties = jsonResponse.data;
-    renderPartyCards(parties)
-
-}catch(err) {
-     console.error(err);
+    renderPartyCards(parties);
+  } catch (err) {
+    console.error(err);
+  }
 }
+
+async function createEvent(event) {
+  try {
+    //pausing to get a response
+    const response = await fetch(EVENTS, {
+      method: "POST",
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify(event),
+    });
+    if (!response.ok) {
+      console.log(`API error`, response);
+
+      return;
+    }
+    const jsonResponse = await response.json();
+    const parties = jsonResponse.data;
+
+    console.log(parties);
+  } catch (err) {
+    console.error(err);
+  }
 }
-
-
-
-
+fetchEvents();
 
 // async function render() {
 //   awaitGuest();
